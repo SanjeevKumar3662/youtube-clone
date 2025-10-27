922eb85a7b402aaa82759fa9408c28e38d75c37a
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-27 18:51:56 +0530

    passing registerUsesr and multer's upload.fields middleware for /register route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,26 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+// this express post() method can accept middelwares and executes them one by one
+router.route("/register").post(
+  //upload.fields is a middelware from multer
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+
+  //this registerUser is my middleware that uploads imgs to
+  // cloudinary and then does a db call to create a user
+  registerUser
+);
 
 export default router;

83f7901471f629d7e6272d659f3fc4401abf7e12
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-27 12:36:06 +0530

    added users/register route

--- src/app.js
+++ src/app.js
@@ -10,4 +10,9 @@ app.use(express.urlencoded({ limit: "16kb", extended: false }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
 export default app;

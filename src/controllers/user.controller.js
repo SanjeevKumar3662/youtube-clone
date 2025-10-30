2695e6b2a16b94a413e424739135c11a35b0aee3
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-10-30 22:11:03 +0530

    added change current password method

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -231,3 +231,25 @@ export const refreshAccessToken = asyncHandler(async (req, res) => {
     );
   }
 });
+
+export const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  if (!(oldPassword && newPassword)) {
+    throw new ApiError(400, "Either old or new password not provided");
+  }
+
+  const user = await User.findById(req.user?._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Old password not correct");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+});
